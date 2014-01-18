40155b5ee76fa2e7d66e78a5ff76500cf4005f83
Gui Lin <guileen@gmail.com>
2014-01-18 23:41:31 +0800

    end
    add detail

--- routes/topic.js
+++ routes/topic.js
@@ -2,9 +2,19 @@ var cclog = require('cclog');
 module.exports = function(app) {
 
   // tiopic view by slug url
-  app.get('/t/:tiopic', function(req, res, next) {
+  app.get('/t/:tid', function(req, res, next) {
+          app.services.topic.getDetail(req.params.tid, function(err, topic) {
+                  if(err) {return callback(err);}
+                  res.format({
+                      json: function() {
+                        res.send(topic)
+                      }
+                    , html: function() {
+                        res.render('topic/detail', {topic: topic});
+                      }
+                  })
+          });
   });
-
 
   // tiopic.
   app.get('/topic/create', function(req, res, next) {
@@ -35,35 +45,5 @@ module.exports = function(app) {
                   res.json({id:data.id, gid: data.gid});
           })
   });
-
-  app.get('/topic/:topicid/step-2', function(req, res, next) {
-      var tid = req.param('topicid');
-      app.services.getJoinedGroups(req.session.user.id, function(err, joinedGroups) {
-          if(err) {
-            cclog.error(err.message);
-            return res.error(err);
-          }
-
-      app.services.topic.get(tid, function(err, topic) {
-          if(err) {
-            cclog.error(err.stack);
-            return res.json({err: err.message});
-          }
-          if(topic.uid != req.session.user.id) {
-            return res.json({err: 'permission denined'});
-          }
-          res.render('topic/step-2', {topic: topic, joinedGroups: joinedGroups});
-      })
-
-      })
-  })
-
-  // tiopic view by id
-  app.get('/topic/:tiopicid', function(req, res, next) {
-          app.services.topic.get(req.params.tiopicid, function(err, data) {
-                  if(err) {return callback(err);}
-                  res.send(data);
-          });
-  })
 
 }
